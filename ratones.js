53278f75af2fd54c67fa90eef82978877602009b
LEANDRO M MELO <30564192+leandrotarefas@users.noreply.github.com>
2023-03-31 21:23:06 -0300

    Update ratones.js

--- ratones.js
+++ ratones.js
@@ -1,5 +1,7 @@
-const net = require('net');
 const fs = require('fs');
+const http = require('http');
+const io = require('socket.io');
+const ioClient = require('socket.io-client');
 const process = require('process');
 
 const args = process.argv.slice(2);
@@ -11,32 +13,31 @@ if (args.length < 4) {
 
 const [portaEntrada, portaDestino, hostDestino, tag] = args;
 
-const server = net.createServer((socket) => {
+const server = http.createServer();
+const socketServer = io(server);
+
+socketServer.on('connection', (socket) => {
   let receivedData = '';
 
-  socket.on('data', (data) => {
+  socket.on('message', (data) => {
     receivedData += data.toString('utf8');
   });
 
-  socket.on('end', () => {
+  socket.on('disconnect', () => {
     console.log(`[${tag}] ${receivedData}`);
     fs.appendFileSync('output.txt', `[${tag}] ${receivedData}\n`, 'utf8');
 
-    const destinationSocket = net.createConnection({ port: parseInt(portaDestino), host: hostDestino }, () => {
-      destinationSocket.write(receivedData);
+    const destinationSocket = ioClient(`${hostDestino}:${portaDestino}`);
+
+    destinationSocket.on('connect', () => {
+      destinationSocket.emit('message', receivedData);
+      destinationSocket.disconnect();
     });
 
-    destinationSocket.on('error', (err) => {
+    destinationSocket.on('connect_error', (err) => {
       console.error(`Erro ao conectar ao hostDestino: ${hostDestino}:${portaDestino} - ${err.message}`);
-    });
-
-    destinationSocket.on('close', () => {
-     
-  console.log(`Dados enviados para ${hostDestino}:${portaDestino}`);
-  destinationSocket.destroy();
 });
-    
-    });
+});
 
 socket.on('error', (err) => {
 console.error(Erro no socket de entrada: ${err.message});
