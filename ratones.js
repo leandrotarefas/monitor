2f1934a185a67ae6c1fbde6edc4f59c06b366642
LEANDRO M MELO <30564192+leandrotarefas@users.noreply.github.com>
2023-03-31 21:49:40 -0300

    Update ratones.js

--- ratones.js
+++ ratones.js
@@ -1,53 +1,36 @@
 const fs = require('fs');
 const http = require('http');
 const io = require('socket.io');
-const ioClient = require('socket.io-client');
 const process = require('process');
 
 const args = process.argv.slice(2);
 
-if (args.length < 4) {
-  console.error('Uso: node script.js portaEntrada portaDestino hostDestino tag');
+if (args.length < 1) {
+  console.error('Uso: node script.js porta');
   process.exit(1);
 }
 
-const [portaEntrada, portaDestino, hostDestino, tag] = args;
+const [porta] = args;
 
 const server = http.createServer();
 const socketServer = io(server);
 
 socketServer.on('connection', (socket) => {
-  let receivedData = '';
-
-  socket.on('message', (data) => {
-    receivedData += data.toString('utf8');
+  socket.onAny((eventName, ...args) => {
+    const eventData = JSON.stringify(args);
+    console.log(`Evento: ${eventName} - Conteúdo: ${eventData}`);
+    fs.appendFileSync('output.txt', `Evento: ${eventName} - Conteúdo: ${eventData}\n`, 'utf8');
   });
 
-  socket.on('disconnect', () => {
-    console.log(`[${tag}] ${receivedData}`);
-    fs.appendFileSync('output.txt', `[${tag}] ${receivedData}\n`, 'utf8');
-
-    const destinationSocket = ioClient(`${hostDestino}:${portaDestino}`);
-
-    destinationSocket.on('connect', () => {
-      destinationSocket.emit('message', receivedData);
-      destinationSocket.disconnect();
-    });
-
-    destinationSocket.on('connect_error', (err) => {
-      console.error(`Erro ao conectar ao hostDestino: ${hostDestino}:${portaDestino} - ${err.message}`);
-});
+  socket.on('error', (err) => {
+    console.error(`Erro no socket: ${err.message}`);
+  });
 });
 
-socket.on('error', (err) => {
-console.error(Erro no socket de entrada: ${err.message});
-});
-});
-
-server.listen(parseInt(portaEntrada), () => {
-console.log(Servidor escutando na porta ${portaEntrada});
+server.listen(parseInt(porta), () => {
+  console.log(`Servidor escutando na porta ${porta}`);
 });
 
 server.on('error', (err) => {
-console.error(Erro no servidor: ${err.message});
+  console.error(`Erro no servidor: ${err.message}`);
 });
